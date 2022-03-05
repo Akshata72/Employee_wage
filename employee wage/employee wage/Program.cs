3381065e851d62d5cf0434051dea6d5deb9a99c7
Akshata72 <sableakshata880@gmail.com>
2022-03-05 10:43:32 +0530

    Added:Present & Absent

---
+++ employee wage/employee wage/Program.cs
@@ -0,0 +1,20 @@
+﻿//UC1
+int present = 1;
+int emp_per_rate = 20;
+int emphr = 0;
+int empwage = 0;
+
+Random random = new Random();
+int employeestatus = random.Next(0, 2);
+if (employeestatus==present)
+{
+    Console.WriteLine("employee is present");
+    emphr = 8;
+}
+else
+{
+    Console.WriteLine("employee is absent");
+    emphr = 0;
+}
+empwage = (emp_per_rate * emphr);
+Console.WriteLine("daily wage" + empwage);
